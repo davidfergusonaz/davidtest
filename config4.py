d2e4891ec1715a4cab726e723f90c782dd58b655
davidfergusonaz <davidfergusonaz@gmail.com>
2017-07-25 21:10:09 -0700

    Create config4.py

---
+++ config4.py
@@ -0,0 +1,12 @@
+# AWS account credentials
+AWS_ACCESS_KEY_ID= AKIAIWJR3KGHCZ3OAXJA
+AWS_SECRET_ACCESS_KEY=  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
+
+##
+# A seed file contains information to populate the database. It contains:
+# - roles, usernames, passwords
+# - integration tokens gained via a successful oauth handshake,
+#   used to communicate with a point-of-sale (Vend) on behalf of retailers
+# - suppliers, outlets
+# - anything and everything which we cannot add via a UI for onboarding warehouse users
+##
