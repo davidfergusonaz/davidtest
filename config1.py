34103b02d93838458cae1d8ff889c2ed3c4a048b
davidfergusonaz <davidfergusonaz@gmail.com>
2017-07-31 21:42:02 -0700

    Update config1.py

--- config1.py
+++ config1.py
@@ -1,5 +1,5 @@
 provider "aws" {
   access_key = "AKIAJCJUB35JFIDR5XWA"
-  secret_key = "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"
+  secret_key = "eDez8kRsqE2fTFaz0HzyZDXudPKDLlRwjcazVTLe"
   region     = "${var.region}
 }
