dfc6cc508d4111050d845bb33d63c74ff162ad21
davidfergusonaz <davidfergusonaz@gmail.com>
2017-08-10 05:24:22 -0700

    Update config5.py

--- config5.py
+++ config5.py
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIAICWUKFYULPZVQ33A"
+  access_key = "AKIAJCOJBDSCXZZ2GVLA"
   secret_key = "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"
   region     = "${var.region}"
 }
